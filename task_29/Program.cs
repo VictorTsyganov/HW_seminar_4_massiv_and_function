dd011f043f5685fc5defe2134497543af931199e
Victor Tsyganov <tsyganov1989@gmail.com>
2022-11-07 14:47:11 +0300

    add_task_29

---
+++ task_29/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+Console.Clear();
+Console.WriteLine($"Задайте размер массива: ");
+int size = int.Parse(Console.ReadLine());
+int[] arr = new int [size];
+
+NewMass(arr);
+Console.WriteLine();
+Console.Write(ReadMass(arr));
+
+void NewMass(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+        Console.Write($"{array[i]} ");
+    }
+}
+
+string ReadMass(int[] array)
+{
+    string text = $"[{array[0].ToString()}";
+    for (int i = 1; i < array.Length; i++)
+    {
+        text = $"{text}, {array[i].ToString()}";
+    }
+    return text + "]";
+}
